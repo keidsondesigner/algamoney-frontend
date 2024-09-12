6812d396d8353eafceadebfd4185df2ca7a429a7
Keidson Roby <keidsondesigner@gmail.com>
2024-09-11 20:52:15 -0400

    chore: atualização import de components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,29 +11,41 @@ import { CalendarModule } from 'primeng/calendar';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
 import { SidebarModule } from 'primeng/sidebar';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { DropdownModule } from 'primeng/dropdown';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 import { LancamentosPesquisaComponent } from './features/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PessoasPesquisaComponent } from './features/pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentosCadastroComponent } from './features/lancamentos/lancamentos-cadastro/lancamentos-cadastro.component';
+import { PessoasCadastroComponent } from './features/pessoas/pessoas-cadastro/pessoas-cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LancamentosPesquisaComponent,
     NavbarComponent,
-    PessoasPesquisaComponent
+    PessoasPesquisaComponent,
+    LancamentosCadastroComponent,
+    PessoasCadastroComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     InputTextModule,
     CalendarModule,
     ButtonModule,
     TableModule,
     TooltipModule,
-    SidebarModule
+    SidebarModule,
+    InputTextareaModule,
+    DropdownModule,
+    SelectButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
