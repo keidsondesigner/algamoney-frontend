fcf896a68b5dfb572d181e5f025cda5a693c0d33
Keidson Roby <keidsondesigner@gmail.com>
2024-09-14 23:49:03 -0400

    chore: movido Módulo Lançamentos e Pessoas para Módulo Features

---
+++ src/app/features/lancamentos/lancamentos.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LancamentosRoutingModule } from './lancamentos-routing.module';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { LancamentosCadastroComponent } from './lancamentos-cadastro/lancamentos-cadastro.component';
+import { PrimengModModule } from 'src/app/shared/primeng-mod/primeng-mod.module';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    LancamentosPesquisaComponent,
+    LancamentosCadastroComponent,
+  ],
+  imports: [
+    CommonModule,
+    LancamentosRoutingModule,
+    PrimengModModule,
+    FormsModule,
+  ]
+})
+export class LancamentosModule { }
