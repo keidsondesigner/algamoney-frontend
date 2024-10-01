b90be39a166fc67619f79503ba0074b493a0b533
Keidson Roby <keidsondesigner@gmail.com>
2024-10-01 18:35:21 -0400

    feat: criado Interceptor AddJwt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { FeaturesModule } from './features/features.module';
 import { SharedModule } from './shared/shared.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AddJwtInterceptor } from './core/interceptors/add-jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -20,11 +22,14 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     SharedModule,
     AuthModule,
     FeaturesModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AddJwtInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
