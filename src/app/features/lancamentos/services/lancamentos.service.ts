706ebfaf95b05bba5a9d385e2c4a37fa119eb032
Keidson Roby <keidsondesigner@gmail.com>
2024-10-03 19:29:40 -0400

    feat: Implementado cache de dados no LancamentosService

--- src/app/features/lancamentos/services/lancamentos.service.ts
+++ src/app/features/lancamentos/services/lancamentos.service.ts
@@ -9,16 +9,31 @@ import { ListarLancamentosResponse } from '../models/user-response.model';
 export class LancamentosService {
   private baseUrl = 'http://localhost:8080';
 
+  private cacheSubject = new BehaviorSubject<{ [key: string]: ListarLancamentosResponse[] }>({});
+  private cache$ = this.cacheSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
   listarLancamentos(): Observable<ListarLancamentosResponse[]> {
-    return this.http.get<ListarLancamentosResponse[]>(`${this.baseUrl}/lancamentos`);
+    return this.cache$.pipe(
+      take(1),
+      map(cache => cache['listar']),
+      switchMap(dados => dados ? of(dados) : this.http.get<ListarLancamentosResponse[]>(`${this.baseUrl}/lancamentos`).pipe(
+        shareReplay(1),
+        tap(dados => this.cacheSubject.next({ ...this.cacheSubject.value, ['listar']: dados }))
+      ))
+    );
   }
   
   buscarPorDescricao(termo: string): Observable<ListarLancamentosResponse[]> {
-    return this.http.get<ListarLancamentosResponse[]>(`${this.baseUrl}/lancamentos/filtrar`, {
-      params: { descricao: termo }
-    });
+    return this.cache$.pipe(
+      take(1),
+      map(cache => cache[termo]),
+      switchMap(dados => dados ? of(dados) : this.http.get<ListarLancamentosResponse[]>(`${this.baseUrl}/lancamentos/filtrar`, {
+        params: { descricao: termo }
+      }).pipe(
+        tap(dados => this.cacheSubject.next({ ...this.cacheSubject.value, [termo]: dados }))
+      ))
+    );
   }
 }
